5cb571ec26af0b75dac12648a742e1f73c8e650b
ammarm08 <ammar.mian08@gmail.com>
2015-12-13 16:37:50 -0500

    initial browserifying of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,18 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var react = require('gulp-react');
 var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
 
 var path = {
   HTML: 'src/index.html',
-  ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
-  JS: ['src/js/Child.js', 'src/js/Parent.js', 'src/js/App.js'],
   MINIFIED_OUT: 'build.min.js',
-  DEST_SRC: 'dist/src',
+  OUT: 'build.js',
+  DEST: 'dist',
   DEST_BUILD: 'dist/build',
-  DEST: 'dist'
+  DEST_SRC: 'dist/src',
+  ENTRY_POINT: './src/js/App.js'
 };
-
-gulp.task('transform', function(){
-  gulp.src(path.JS)
-    .pipe(react())
-    .pipe(gulp.dest(path.DEST_SRC));
-});
-
-gulp.task('copy', function(){
-  gulp.src(path.HTML)
-    .pipe(gulp.dest(path.DEST));
-});
-
-gulp.task('watch', function(){
-  gulp.watch(path.ALL, ['transform', 'copy']);
-});
-
-gulp.task('build', function(){
-  gulp.src(path.JS)
-    .pipe(react())
-    .pipe(concat(path.MINIFIED_OUT))
-    .pipe(uglify())
-    .pipe(gulp.dest(path.DEST_BUILD));
-});
-
-gulp.task('replaceHTML', function(){
-  gulp.src(path.HTML)
-    .pipe(htmlreplace({
-      'js': 'build/' + path.MINIFIED_OUT
-    }))
-    .pipe(gulp.dest(path.DEST));
-});
-
-gulp.task('default', ['watch']);
-
-gulp.task('production', ['replaceHTML', 'build']);
