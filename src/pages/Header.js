4411044fad0e8ede1d9451807d5600862c89920e
Gregg PollackAshley <ashleydorris89@gmail.com>
2015-12-28 01:05:49 -0800

    basic functionality

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { LoginLink, LogoutLink, Authenticated, NotAuthenticated } from 'react-stormpath';
+
+import { LoginLink, LogoutLink, NotAuthenticated, Authenticated } from 'react-stormpath';
+
 
 export default class Header extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-default navbar-static-top">
                 <div className="container">
+                    <div className="navbar-header">
+                        <button type="button" data-toggle="collapse" data-target="#navbar-collapse" className="navbar-toggle collapsed">
+                            <span className="sr-only">Toggle Navigation</span>
+                            <span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span>
+                        </button>
+                    </div>
                     <div id="navbar-collapse" className="collapse navbar-collapse">
                         <ul className="nav navbar-nav">
                             <li><Link to="/">Home</Link></li>
@@ -22,16 +30,16 @@ export default class Header extends React.Component {
                                     <LoginLink />
                                 </li>
                             </NotAuthenticated>
-                            <NotAuthenticated>
-                                <li>
-                                    <Link to="/register">Create Account</Link>
-                                </li>
-                            </NotAuthenticated>
                             <Authenticated>
                                 <li>
                                     <LogoutLink />
                                 </li>
                             </Authenticated>
+                            <NotAuthenticated>
+                                <li>
+                                    <Link to="/registration">Create Account</Link>
+                                </li>
+                            </NotAuthenticated>
                         </ul>
                     </div>
                 </div>
